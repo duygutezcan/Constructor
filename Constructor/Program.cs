2cdec6d234dc828f66c7f3335e7f61836d7a74fb
SEM <SEM@DESKTOP-4GKM6HK>
2022-11-08 09:03:42 +0300

    Add project files.

---
+++ Constructor/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Constructor;
+Ogrenci o = new Ogrenci();
+o.Ad = "Duygu";
+o.Soyad = "Tezcan Kantar";
+o.Id = 1;
+Console.WriteLine(o.Ad + " " + o.Soyad + " "  + o.KayitTarih);
